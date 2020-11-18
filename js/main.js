511d2ad5f7bacdc1e901aada0cf2b33e2973e20d
Tim Scully <tim.scully@wundermanthompson.com>
2020-11-18 19:12:50 +0000

    added smooth scroller and cbackground change for work page

--- js/main.js
+++ js/main.js
@@ -1,23 +1,43 @@
-var one = "#10e88a",
-two = "#7f3995",
-three = "#0d78e7",
-four = "#f88f18",
-five = "#bb6774";
-
 $(window).on("scroll touchmove", function() {
-    if ($(document).scrollTop() >= $("#one").position().top) {
-        $('body').css('background', $("#one").attr("data-color"));
+    if ($(document).scrollTop() >= $("#intro-section").position().top) {
+        $('body').css('background', $("#intro-section").attr("data-color"));
     };
-    if ($(document).scrollTop() > $("#two").position().top) {
-        $('body').css('background', $("#two").attr("data-color"))
+    if ($(document).scrollTop() > $("#work-section").position().top) {
+        $('body').css('background', $("#work-section").attr("data-color"))
     };
-    if ($(document).scrollTop() > $("#three").position().top) {
-        $('body').css('background', $("#three").attr("data-color"))
-    };
-    if ($(document).scrollTop() > $("#four").position().top) {
-        $('body').css('background', $("#four").attr("data-color"))
-    };
-    if ($(document).scrollTop() >= $("#five").position().top) {
-        $('body').css('background', $("#five").attr("data-color"))
-    };
+//    if ($(document).scrollTop() > $("#three").position().top) {
+//        $('body').css('background', $("#three").attr("data-color"))
+//    };
+//    if ($(document).scrollTop() > $("#four").position().top) {
+//        $('body').css('background', $("#four").attr("data-color"))
+//    };
+//    if ($(document).scrollTop() >= $("#five").position().top) {
+//        $('body').css('background', $("#five").attr("data-color"))
+//    };
+});
+
+
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top + 1
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
 });
