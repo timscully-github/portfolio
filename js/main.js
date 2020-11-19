5dc24177147c8cded9f2fd1328ad25708ea805de
Tim Scully <tim.scully@wundermanthompson.com>
2020-11-19 12:34:08 +0000

    fixed bug in scroll to anchor

--- js/main.js
+++ js/main.js
@@ -17,27 +17,10 @@ $(window).on("scroll touchmove", function() {
 });
 
 
-$(document).ready(function() {
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top + 10
-            }, 800, function() {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        }
-    });
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top + 1
+    }, 800);
 });
